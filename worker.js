48633a51253a72fa6aa9cbd2546a381dcb15e476
kjh9410065-art <kjh9410065@gmail.com>
2026-09-17 08:12:10 +0900

    seo: sync served sitemap homepage lastmod

--- worker.js
+++ worker.js
@@ -28,7 +28,7 @@ export default {
     if (url.pathname === "/sitemap.xml") {
       const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url><loc>https://mingka.tcflick.com/</loc><lastmod>2026-09-13</lastmod><changefreq>weekly</changefreq><priority>1.0</priority></url>
+<url><loc>https://mingka.tcflick.com/</loc><lastmod>2026-09-16</lastmod><changefreq>weekly</changefreq><priority>1.0</priority></url>
 <url><loc>https://mingka.tcflick.com/long-term-rental-vs-lease.html</loc><lastmod>2026-09-13</lastmod><changefreq>monthly</changefreq><priority>0.8</priority></url>
 <url><loc>https://mingka.tcflick.com/car-purchase-vs-rental.html</loc><lastmod>2026-09-13</lastmod><changefreq>monthly</changefreq><priority>0.8</priority></url>
 <url><loc>https://mingka.tcflick.com/long-term-rental-guide.html</loc><lastmod>2026-09-13</lastmod><changefreq>monthly</changefreq><priority>0.8</priority></url>
